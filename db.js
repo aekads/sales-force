380f3f4b1f7845397965d49bf29f18f7d097b4c0
dhvanil1403 <pd14030304@gmail.com>
2025-06-13 16:15:26 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,24 @@
+// config/db.js
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: "u3m7grklvtlo6",
+    host: "35.209.89.182",
+    database: "dbzvtfeophlfnr",
+    password: "AekAds@24",
+    port: 5432,
+     ssl: false
+});
+
+module.exports = pool;
+
+
+
+
+
+
+
+
+                                                                                    
+
+                                                                            
